1d9feda3c881c42f99b85341ee9cddee3ac226ee
Leo <scau_lzc@163.com>
2020-08-06 09:02:29 +0800

    修改了druid的配置方式

--- springboot_druid/src/main/java/com/druid/DruidApplication.java
+++ springboot_druid/src/main/java/com/druid/DruidApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootDruidApplication {
+public class DruidApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootDruidApplication.class, args);
+        SpringApplication.run(DruidApplication.class, args);
     }
 
 }
