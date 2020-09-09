0c6a1f99797a0a056e6b921ee1e8a83c5a12b35d
Leo <scau_lzc@163.com>
2020-09-09 17:08:54 +0800

    更加精细控制出现线程安全的情况

--- thread/src/main/java/com/leo/thread/cas/CASTest.java
+++ thread/src/main/java/com/leo/thread/cas/CASTest.java
@@ -10,7 +10,7 @@ public class CASTest {
     private static int not_safe_count = 0;
     private static int safe_count = 0;
     private static final AtomicInteger count = new AtomicInteger(0);
-    private static final int times = 100000;
+    private static final int times = 100;
     private static final int threadNums = 2;
 
     public static void main(String[] args) {
@@ -25,11 +25,16 @@ public class CASTest {
             new Thread(() -> {
                 //每个线程让count自增 100000 次
                 for (int i1 = 0; i1 < times; i1++) {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     count.incrementAndGet();
                 }
             }).start();
         }
-        TimeUnit.SECONDS.sleep(1);
+        TimeUnit.SECONDS.sleep(2);
         System.out.println("cas: " + count);
     }
 
@@ -39,14 +44,19 @@ public class CASTest {
             new Thread(() -> {
                 //每个线程让count自增 100000 次
                 for (int i1 = 0; i1 < times; i1++) {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     synchronized (CASTest.class) {
                         safe_count++;
                     }
                 }
             }).start();
         }
-        TimeUnit.SECONDS.sleep(1);
-        System.out.println("synchronized: " +safe_count); //等于 times * threadNums
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("synchronized: " + safe_count); //等于 times * threadNums
     }
 
     @SneakyThrows
@@ -55,11 +65,16 @@ public class CASTest {
             new Thread(() -> {
                 //每个线程让 count 自增 100000 次
                 for (int i1 = 0; i1 < times; i1++) {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     not_safe_count++;
                 }
             }).start();
         }
-        TimeUnit.SECONDS.sleep(1);
+        TimeUnit.SECONDS.sleep(2);
         System.out.println(not_safe_count); // 不等于 times * threadNums
     }
 }
